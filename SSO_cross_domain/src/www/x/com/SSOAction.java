6bdd7e087beb061c259a32c2668b2a1b5c9114bf
lxlx704034204 <704034204@qq.com>
2017-09-15 20:11:31 +0800

    1

--- SSO_cross_domain/src/www/x/com/SSOAction.java
+++ SSO_cross_domain/src/www/x/com/SSOAction.java
@@ -82,9 +82,11 @@ public class SSOAction extends ActionSupport {
 	public String checkcookie() throws IOException {
 		boolean ok = SSOCheck.checkcookie(cookiename, cookieval);
 		String result = "0";
+		// 存储cookie 跨域的话，就不能在此处setCookie了！！！：因为在此处设置的cookie是属于.x.com域的cookie，没有意义！
+		//so，只能在各自的子action中设置各自的cookie！！！：相当于a、b两人在街上逛街，a突然看到个很喜欢的东西想买，但下一刻发现没带钱和银行卡，但b也只有银行卡，此时b不可能把卡和卡的密码都给a！a可以请b帮忙付钱！
 		if(ok){
 			result = "1";
-		}
+		}	//服务器间通信以IO的形式返回(只提供功能，不提供页面)，没有return!!!
 		HttpServletResponse response = ServletActionContext.getResponse();
 		response.getWriter().print(result);
 		response.getWriter().close();
